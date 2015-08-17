d5f6fe29ed03da525571ea89da229fb179da7ca6
PizzaKirby <pizzakirby@googlemail.com>
2015-08-17 16:58:00 +0200

    minor changes

--- banlist/banlist.lua
+++ banlist/banlist.lua
@@ -43,6 +43,7 @@ function bl.loghook(line)
 		elseif(line:match("#?#End of banlist#?#")) then
 			bl.tbanlist.expected =  nil
 			local id = line:match("#?#End of banlist#?#(%d*)")
+			
 			parse('cmsg "©'..bl.colors.orange..'Total bancount : '..bl.tbanlist.ctotal..'" '..id)
 			parse('cmsg "©'..bl.colors.orange..'Amount of '..bl.tbanlist.btype:sub(2)..' bans : '..bl.tbanlist.ctype..'" '..id)
 			for _,v in ipairs(bl.tbanlist.list) do
