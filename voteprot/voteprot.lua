ceac2a5ba485acdc7af4f50eef2a019ccdda131a
PizzaKirby <pizzakirby@googlemail.com>
2015-08-19 04:01:26 +0200

    fixed a bug

--- voteprot/voteprot.lua
+++ voteprot/voteprot.lua
@@ -6,16 +6,20 @@ vp.DEFAULT_KICK_RATIO = game("mp_kickpercent")
 function vp.vote(src,mode,id)
 	if(mode == 1) then
 
-		if(player(id,"bot")) then return end
+		--if(player(id,"bot")) then return end
 
-		parse("mp_kickpercent "..vp.DEFAULT_KICK_RATIO)
+		id = tonumber(id)
 
 		if(not ALLOW_MULTIPLE_VOTES_BY_SAME_IP) then
 			for _,p in ipairs(player(0,"table")) do
-				if( ( player(p,"ip") == player(src,"ip") ) and ( player(p,"votekick") == id ) ) then parse("mp_kickpercent 0.0") end
+				if( ( player(p,"ip") == player(src,"ip") ) and ( player(p,"votekick") == id ) ) then parse("mp_kickpercent 0.0") timer(20,"vp.reset") end
 			end
 		end
 	end
+end
+
+function vp.reset()
+	parse("mp_kickpercent "..vp.DEFAULT_KICK_RATIO)
 end
 
 addhook("vote","vp.vote",99999)
