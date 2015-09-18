a85111a6ab82617109447bf86938681a5b8ecd22
PizzaKirby <pizzakirby@googlemail.com>
2015-09-18 02:46:25 +0200

    added lua 5.2 version

--- Snippets/table_merge.lua
+++ Snippets/table_merge.lua
@@ -3,6 +3,14 @@
 	@param	Table 	: table to append
 ]]
 
+--pre 5.2
 function table_merge(t1,t2)
 	table.foreach(t2,function(k,v) table.insert(t1,v) end)
+end
+
+--5.2
+function table_merge2(t1,t2)
+	for k,v in ipairs(t2) do
+		table.insert(t1,v)
+	end
 end
